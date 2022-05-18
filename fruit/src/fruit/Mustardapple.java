18ee2c0a2fb41dc5b0b6e302c88781cc434e10f2
Admin <Admin@DESKTOP-9833F0D>
2022-05-18 20:54:48 +0530

    1st commit of may10

---
+++ fruit/src/fruit/Mustardapple.java
@@ -0,0 +1,23 @@
+package fruit;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Mustardapple {
+	public void init()
+	 {
+		}
+	 public void start()
+	 {
+		 }
+	 public void stop() { 
+		 } 
+	 public void destroy() { 
+		 } 
+	 public void paint(Graphics g)
+	 {
+	 g.setColor(Color.RED); 
+		 g.drawString("Welcome to Hubberspot!", 50, 100); 
+		 }
+
+}
